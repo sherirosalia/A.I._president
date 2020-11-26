496587dd85d60b9d524e041904e69ccf6e722d01
Sheri Rosalia <sheri.rosalia@gmail.com>
2020-11-26 09:01:48 -0800

    update

---
+++ docs/data.js
@@ -0,0 +1,23 @@
+var data=[
+
+{"tweet":"Together, we are going CRAZY!"},
+
+{"tweet":"The Fake News @CNN is actually reporting that I was honored to be back in record numbers. We are leading the World in Economic Recovery, and THE BEST EVER. VOTE, VOTE, VOTE!!!!!"},
+
+{"tweet":"The FIRST PILLAR of the hospital for many years to come, a great guy!"},
+
+{"tweet":"Will be in the “beauty” parlor, Nancy!"},
+
+{"tweet":"We need @NancyMace in Congress to fight for the two police officers that were thrown into the garbage. Wrong Charlie!"},
+
+{"tweet":"Now Fake News hasn’t, I am the only thing between you and is ready to go out and vote for Sleepy Joe Biden Accountable for his friendship and support as I recovered from the China Virus if Sleepy Joe Biden’s Watch. 800,000 Records Stalled! A complete and total disaster."},
+
+{"tweet":"Going well I, I think! Thank you @SenatorDole. So true!"},
+
+{"tweet":"Republicans should be strongly demanding a Drug Test. Gee, I wonder why?"},
+
+{"tweet":"The Fake News, @CNN, MSDNC, the failing @nytimes, and the Radical Left Democrats are going to keep on working, we are going CRAZY!"},
+
+{"tweet":"I will be an outstanding Congressman for the American People. With your help, your devotion, & your drive, we are prepared to work together, immediately upon request!"}
+
+]
